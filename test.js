aa4132d70e408f7cc6713157216ca2222c6f94d6
sapho-sys <saphonkunzi@gmail.com>
2022-04-19 15:41:14 +0200

    almost

--- test.js
+++
@@ -1,8 +0,0 @@
-describe('this test' , function(){
-    it('should show you how to use MochaJS' , function(){
-        
-    assert.equal(greet('Bob'), 'Hello, Bob');
-    assert.equal(greet('Sam'), 'Hello, Sam');
-    });
-
-});
