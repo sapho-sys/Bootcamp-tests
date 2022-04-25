7ada81fad1b05c55ab7c86a70c023d5e1a75fa11
sapho-sys <saphonkunzi@gmail.com>
2022-04-25 12:20:34 +0200

    complete!

--- tests/countAllfromPaarl.tests.js
+++ tests/countAllfromPaarl.tests.js
@@ -9,7 +9,7 @@ describe("Testing countAllfromPaarl function", function(){
     it("this function will return 2 for registration plates from Paarl", function(){
         assert.equal(2, countAllPaarl('CJ 345 123, CK 345, CJ 123'));
     })
-    it("this function will return 0 if theres are no plates for requested/blank ", function(){
+    it("this function will return 0 if theres are no plates for blank searches", function(){
         assert.equal(0, countAllPaarl(''));
     })
 });
